4fb7455d606da9a8dccc385c61c5e0f8922dcc1a
Zachary Sutton <zachary-sutton@outlook.com>
2020-03-20 13:08:45 +0000

    Updates

---
+++ precache-manifest.dc1c1e1f4342f17b2588b65ddd983265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c394f9778bad56ee5370c252b5a39dd4",
+    "url": "/drinks/index.html"
+  },
+  {
+    "revision": "cb8967ac45ce59d4c122",
+    "url": "/drinks/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e12c70444a017ab8e31c",
+    "url": "/drinks/static/css/main.909b5ce2.chunk.css"
+  },
+  {
+    "revision": "cb8967ac45ce59d4c122",
+    "url": "/drinks/static/js/2.9784743e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/drinks/static/js/2.9784743e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e12c70444a017ab8e31c",
+    "url": "/drinks/static/js/main.02856add.chunk.js"
+  },
+  {
+    "revision": "67df8b5241a463b1447e",
+    "url": "/drinks/static/js/runtime-main.43350c8e.js"
+  }
+]);
